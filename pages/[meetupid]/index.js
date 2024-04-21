2e40ef5add75fba27b10b68d74619efbec82026c
Rajath Rao <raorajath1998@gmail.com>
2024-04-21 13:38:13 +0530

    Prerendering the posers of NextJs

--- pages/[meetupid]/index.js
+++ pages/[meetupid]/index.js
@@ -11,5 +11,46 @@ const MeetupDetails = () => {
     />
   );
 };
+export async function getStaticPaths() {
+  //fetch data for a single meetup
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupid: "m1",
+        },
+      },
+      {
+        params: {
+          meetupid: "m2",
+        },
+      },
+      {
+        params: {
+          meetupid: "m3",
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  //fetch data fro a single meetup
+  const meetupId = context.params.meetupid;
+  console.log("in getstaticprops|||", context, meetupId);
+  return {
+    props: {
+      meetupDate: {
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
+        id: meetupId,
+        title: "first meetup",
+        address: "Some Street 5, Some City",
+        description: "This is a first meetup",
+      },
+    },
+  };
+}
 
 export default MeetupDetails;
--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,55 @@
 import React from "react";
 import MeetupList from "../components/meetups/MeetupList";
 
-const HomePage = () => {
-  const DUMMY_MEETUPS = [
-    {
-      id: "m1",
-      title: "A First Meetup",
-      image:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
-      address: "Some Address 5, 12345 some city",
-      description: "This is a first meetup",
-    },
-    {
-      id: "m2",
-      title: "A Second Meetup",
-      image:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
-      address: "Some Address 10, 12345 some city",
-      description: "This is a second meetup",
-    },
-  ];
-  return <MeetupList meetups={DUMMY_MEETUPS} />;
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "A First Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
+    address: "Some Address 5, 12345 some city",
+    description: "This is a first meetup",
+  },
+  {
+    id: "m2",
+    title: "A Second Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
+    address: "Some Address 10, 12345 some city",
+    description: "This is a second meetup",
+  },
+];
+
+const HomePage = (props) => {
+  // const [loadedMeetups,setLoadedMeetups]=useState([])
+  // useEffect(()=>{
+  //   //send a http request and feth data
+
+  //   setLoadedMeetups(DUMMY_MEETUPS)
+  // },[])
+  return <MeetupList meetups={props.meetups} />;
 };
+
+// export async function getServerSideProps(context) {
+//   const req = context.req;
+//   const res = context.res;
+
+//   //fetch data from an API
+//   return {
+//     props: {
+//       meetups: DUMMY_MEETUPS,
+//     },
+//   };
+// }
+
+export async function getStaticProps(){
+  //fetch data from an API
+  return {
+    props: {
+      meetups: DUMMY_MEETUPS
+    },
+    revalidate: 10
+  };
+}
 
 export default HomePage;
